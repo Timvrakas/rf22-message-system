ac9ca290e4f43e1fe1fecda89e7857632ab0d285
Tim Vrakas <tim.vrakas@gmail.com>
2015-06-15 14:43:31 -0500

    Added LED debug to selftest/setup sketch

--- eeprom-setup/eeprom-setup.ino
+++ eeprom-setup/eeprom-setup.ino
@@ -1,12 +1,21 @@
 #include <EEPROM.h>
 
+uint8_t greenLED = 6;
+uint8_t redLED = 8;
+
 void setup(){
-EEPROM.write(0,3);
+EEPROM.write(0,2);
+pinMode(greenLED, OUTPUT);
+pinMode(redLED, OUTPUT);
 }
 
-
 void loop(){
-
+digitalWrite(greenLED, HIGH);
+digitalWrite(redLED, LOW);
+delay(500);
+digitalWrite(greenLED, LOW);
+digitalWrite(redLED, HIGH);
+delay(500);
 }
 
 
